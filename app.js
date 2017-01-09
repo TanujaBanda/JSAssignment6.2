23ab00ad11a13f2211d633d489f425169c4fc0e1
TanujaBanda <tanujakumari24m@gmail.com>
2017-01-09 13:24:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var a=6,b=9,c=5;
+if(a>b && a>c)
+	console.log("The greatest number is a"+" "+a);
+else if(b>a && b>c)
+	console.log("The greatest number is b"+" "+b);
+     else
+	console.log("The greatest number is c"+" "+c);
